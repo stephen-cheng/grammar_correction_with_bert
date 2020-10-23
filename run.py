fa9dd2f7e6c3d3cd6d458382e50faa0548b3d25a
steven-cheng-com <jetrobert19@gmail.com>
2020-10-23 22:12:37 +0800

    process

--- run.py
+++ run.py
@@ -1,16 +1,49 @@
 import os
 # os.environ['CUDA_VISIBLE_DEVICES'] = '0'
 os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
-
-from transformers import BertForSequenceClassification
-from transformers import BertTokenizer
-import torch
-
 import pandas as pd
 from spelling_correction_with_seq2seq_learning.utils import CharacterTable, transform
 from spelling_correction_with_seq2seq_learning.utils import restore_model, decode_sequences
 from spelling_correction_with_seq2seq_learning.utils import read_text, tokenize
 
+from transformers import BertForSequenceClassification
+from transformers import BertTokenizer
+import torch
+
+
+def spell_correction_model(model_path, data_path, corpus, test_sentence):
+	error_rate = 0.6
+	reverse = True
+	hidden_size = 512
+	sample_mode = 'argmax'
+	test_sentence = test_sentence.lower()
+	text = read_text(data_path, corpus)
+	vocab = tokenize(text)
+	vocab = list(filter(None, set(vocab)))
+	# `maxlen` is the length of the longest word in the vocabulary
+	# plus two SOS and EOS characters.
+	maxlen = max([len(token) for token in vocab]) + 2
+	train_encoder, train_decoder, train_target = transform(
+	    vocab, maxlen, error_rate=error_rate, shuffle=False, train=False)
+
+	tokens = tokenize(test_sentence)
+	tokens = list(filter(None, tokens))
+	nb_tokens = len(tokens)
+	misspelled_tokens, _, target_tokens = transform(
+	    tokens, maxlen, error_rate=error_rate, shuffle=False, train=False)
+
+	input_chars = set(' '.join(train_encoder))
+	target_chars = set(' '.join(train_decoder))
+	input_ctable = CharacterTable(input_chars)
+	target_ctable = CharacterTable(target_chars)
+
+	encoder_model, decoder_model = restore_model(model_path, hidden_size)
+	input_tokens, target_tokens, decoded_tokens = decode_sequences(
+    	misspelled_tokens, target_tokens, input_ctable, target_ctable, 
+    	maxlen, reverse, encoder_model, decoder_model, nb_tokens,
+    	sample_mode=sample_mode, random=False)
+
+	return input_tokens, decoded_tokens
 
 
 def grammar_checker_model(output_dir, sent):
@@ -57,59 +90,24 @@ def grammar_checker_model(output_dir, sent):
 	  return 1
 
 
-def spell_correction_model(model_path, data_path, corpus, test_sentence):
-	error_rate = 0.6
-	reverse = True
-	hidden_size = 512
-	sample_mode = 'argmax'
-	test_sentence = test_sentence.lower()
-	text = read_text(data_path, corpus)
-	vocab = tokenize(text)
-	vocab = list(filter(None, set(vocab)))
-	# `maxlen` is the length of the longest word in the vocabulary
-	# plus two SOS and EOS characters.
-	maxlen = max([len(token) for token in vocab]) + 2
-	train_encoder, train_decoder, train_target = transform(
-	    vocab, maxlen, error_rate=error_rate, shuffle=False, train=False)
-
-	tokens = tokenize(test_sentence)
-	tokens = list(filter(None, tokens))
-	nb_tokens = len(tokens)
-	misspelled_tokens, _, target_tokens = transform(
-	    tokens, maxlen, error_rate=error_rate, shuffle=False, train=False)
-
-	input_chars = set(' '.join(train_encoder))
-	target_chars = set(' '.join(train_decoder))
-	input_ctable = CharacterTable(input_chars)
-	target_ctable = CharacterTable(target_chars)
-
-	encoder_model, decoder_model = restore_model(model_path, hidden_size)
-	input_tokens, target_tokens, decoded_tokens = decode_sequences(
-    	misspelled_tokens, target_tokens, input_ctable, target_ctable, 
-    	maxlen, reverse, encoder_model, decoder_model, nb_tokens,
-    	sample_mode=sample_mode, random=False)
-
-	return input_tokens, decoded_tokens
-
-
 if __name__ == '__main__':
-
-	output_dir = 'grammar_checker_with_bert/model/'
 	test_sent = "you doin good work"
-	flag = grammar_checker_model(output_dir, test_sent)
-
-	if flag:
-		model_path = 'spelling_correction_with_seq2seq_learning/models/seq2seq.h5'
-		data_path = 'spelling_correction_with_seq2seq_learning/data'
-		corpus = ['train.txt', 'character.txt']
-		input_tokens, decoded_tokens = spell_correction_model(model_path, data_path, corpus, test_sent)
-	else:
-
-		input_tokens = decoded_tokens = test_sent
-	print('---------------------****---------------------')
+	# spelling correction
+	model_path = 'spelling_correction_with_seq2seq_learning/models/seq2seq.h5'
+	data_path = 'spelling_correction_with_seq2seq_learning/data'
+	corpus = ['train.txt', 'character.txt']
+	input_tokens, decoded_tokens = spell_correction_model(model_path, data_path, corpus, test_sent)
+	print('---------------------Spelling Correction---------------------')
 	print('Input sentence:  ', ' '.join([token for token in input_tokens]))
 	print('Corrected sentence:', ' '.join([token for token in decoded_tokens]))
+	new_sent = decoded_tokens
 
-
+	# grammar checker
+	output_dir = 'grammar_checker_with_bert/model/'
+	flag = grammar_checker_model(output_dir, new_sent)
+	if flag:
+		print("Gramatically in-correct")
+	else:
+		print("Gramatically correct")
 
 
